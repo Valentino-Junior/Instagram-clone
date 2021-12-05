05b50acbe13f1cdc47e340d4fd77f7c6850ed6c3
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-12-05 14:41:56 +0300

    Added class forms

---
+++ clone/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Profile, Image, Comment
+
+class RegistrationForm(UserCreationForm):
+
+    class Meta:
+        model=User
+        fields=["username", "email", "password1", "password2"]
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model=Image
+        exclude=['comments','likes','profile']
+
+class ImageCommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=["comment"]
+
+class UpdateUserProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=["bio"]
+        
