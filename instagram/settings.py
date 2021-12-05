28fda05decca0df8d13d9fea1898089dc2b474cf
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-12-05 15:46:15 +0300

    Added email configurations

--- instagram/settings.py
+++ instagram/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
