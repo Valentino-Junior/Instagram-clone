40b16e1c9d9afdedd125219ab54589f8d6a491c2
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-12-05 14:50:24 +0300

    Added tinymyc path

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-     path('', include('clone.urls')),
+    path('', include('clone.urls')),
+    path("tinymce/", include('tinymce.urls')),
 ]
